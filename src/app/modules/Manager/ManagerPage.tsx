04a049691154ad34885cb2cfb4603d7eded7627b
Stephen <stolak>
2023-10-26 17:13:07 +0100

    first commit

---
+++ src/app/modules/Manager/ManagerPage.tsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import { Navigate, Outlet, Route, Routes } from 'react-router-dom'
+import { PageLink, PageTitle } from '../../../_metronic/layout/core'
+import { Folders } from './components/Folders'
+import { FIles } from './components/FIles'
+import { ManagerHeader } from './ManagerHeader'
+
+const accountBreadCrumbs: Array<PageLink> = [
+    {
+        title: 'File Manager',
+        path: '/file-manager/folders',
+        isSeparator: false,
+        isActive: false,
+    },
+    {
+        title: '',
+        path: '',
+        isSeparator: true,
+        isActive: false,
+    },
+]
+
+const ManagerPage: React.FC = () => {
+    return (
+        <Routes>
+            <Route
+                element={
+                    <>
+                        <Folders />
+                        <Outlet />
+                    </>
+                }
+            >
+                <Route
+                    path='folders'
+                    element={
+                        <>
+                            <PageTitle breadcrumbs={accountBreadCrumbs}>Folders</PageTitle>
+                            <Folders />
+                        </>
+                    }
+                />
+                <Route
+                    path='files'
+                    element={
+                        <>
+                            <PageTitle breadcrumbs={accountBreadCrumbs}>FIles</PageTitle>
+                            <FIles />
+                        </>
+                    }
+                />
+                <Route index element={<Navigate to='/file-manager/folders' />} />
+            </Route>
+        </Routes>
+    )
+}
+
+export default ManagerPage
---
+++ src/app/pages/filemanager/CreateFolder.tsx
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+
+function CreateNewFolder({ show, onHide }) {
+    const [folderName, setFolderName] = useState('');
+
+    // const handleNewFolderClick = () => {
+    //     setShowModal(true);
+    // };
+
+    // const handleModalClose = () => {
+    //     setShowModal(false);
+    //     setFolderName(''); // Clear the folder name input
+    // };
+
+    // const handleFolderNameChange = (e) => {
+    //     setFolderName(e.target.value);
+    // };
+
+
+    const handleCreateFolder = () => {
+        // Define the folder name and any other data you need to send
+        const newFolder = {
+            name: folderName,
+        };
+
+        // Perform the POST request to create the folder
+        fetch('https://api.example.com/folders', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newFolder),
+        }).then((response) => {
+            if (response.ok) {
+                // Folder created successfully
+                return response.json();
+            } else {
+                throw new Error('Failed to create folder');
+            }
+        }).then((createdFolder) => {
+            // Handle the created folder data as needed
+            console.log('Folder created:', createdFolder);
+
+            // After creating the folder, close the modal
+            setFolderName('');
+
+        }).catch((error) => {
+            console.error('Error creating folder:', error);
+            // Handle the error (e.g., display an error message)
+        });
+    };
+
+    return (
+        <Modal show={show} onHide={onHide}>
+            <Modal.Header closeButton>
+                <Modal.Title>Create a New Folder</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <form>
+                    <div className="mb-3">
+                        <label htmlFor="folderName" className="form-label">
+                            Folder Name:
+                        </label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            id="folderName"
+                            value={folderName}
+                            onChange={(e) => setFolderName(e.target.value)}
+                        />
+                    </div>
+                </form>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={onHide}>
+                    Cancel
+                </Button>
+                <Button variant="primary" onClick={handleCreateFolder}>
+                    Create
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    );
+}
+
+export default CreateNewFolder;
